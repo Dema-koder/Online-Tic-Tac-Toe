644a68959d746534faf06b6afdbae80d6618cff5
Dema-koder <asotruzikovic@gmail.com>
2024-05-11 06:25:49 +0000

    fix style

--- game/tests.py
+++ game/tests.py
@@ -9,30 +9,24 @@ class GameViewTestCase(TestCase):
         self.user2 = User.objects.create_user(username='user2', password='password')
 
     def test_connect_to_game(self):
-        # Создаем игру
         game = Game.objects.create(cross_id=self.user1, whose_move=self.user1)
 
-        # Проверяем, что пользователь user2 может подключиться к игре
         self.client.login(username='user2', password='password')
         response = self.client.post('/connect_to_game/{}/'.format(game.id))
-        self.assertEqual(response.status_code, 200)  # Проверяем, что страница успешно загружена
+        self.assertEqual(response.status_code, 200)  
         game.refresh_from_db()
-        self.assertEqual(game.nought_id, self.user2)  # Проверяем, что пользователь user2 становится ноликами
+        self.assertEqual(game.nought_id, self.user2)  
 
-        # Проверяем, что пользователь не может подключиться к игре, если игра уже полностью заполнена
         game = Game.objects.create(cross_id=self.user1, nought_id=self.user2, whose_move=self.user1)
         response = self.client.post('/connect_to_game/{}/'.format(game.id))
-        self.assertEqual(response.status_code, 200)  # Проверяем, что происходит редирект
+        self.assertEqual(response.status_code, 200)  
 
     def test_make_move(self):
-        # Создаем игру
         game = Game.objects.create(cross_id=self.user1, whose_move=self.user1)
 
-        # Проверяем, что пользователь может сделать ход
         self.client.login(username='user1', password='password')
         response = self.client.post('/make_move/{}/'.format(game.id), {'move': '1'})
-        self.assertEqual(response.status_code, 404)  # Проверяем, что происходит редирект
+        self.assertEqual(response.status_code, 404) 
 
-        # Проверяем, что пользователь не может сделать ход в неверном формате
         response = self.client.post('/make_move/{}/'.format(game.id), {'move': 'invalid'})
-        self.assertEqual(response.status_code, 404)  # Проверяем, что происходит редирект
+        self.assertEqual(response.status_code, 404)  
